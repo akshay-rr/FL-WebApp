89216792066eb72185a020ff472dd0f7b1af1ed3
akshay-rr <akshayrajagopal10@gmail.com>
2022-09-21 19:23:27 +0530

    Added input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [N, setN] = useState('');
+    const [M, setM] = useState('');
+
+    return (
+        <div className="App">
+            <div>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <div className="input-group input-group-sm mb-3">
+                            <span className="input-group-text" id="inputGroup-sizing-sm">M</span>
+                            <input type="number" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                onChange={(e) => setN(e.currentTarget.value)} />
+                        </div>
+                        <div className="input-group input-group-sm mb-3">
+                            <span className="input-group-text" id="inputGroup-sizing-sm">N</span>
+                            <input type="number" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" 
+                                onChange={(e) => setM(e.currentTarget.value)} />
+                        </div>
+                        <button className='btn btn-primary'
+                            onClick={() => alert(N + ' ' + M)}>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
